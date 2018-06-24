9a3926c54a63a69c5e0109585c8522a8148e864d
Oliver Wilkins <okwilkins@googlemail.com>
2018-06-24 23:28:37 +0100

    [Fix] Now using context managers for images

--- main.py
+++ main.py
@@ -14,10 +14,6 @@ CheckDirectoryExists(workingDirectory + "/output")
 CheckDirectoryExists(workingDirectory + "/logos")
 inputFiles = GetInputFiles()
 
-if CheckImageExists(workingDirectory + "/logos/logo.png"):
-	logo = Image.open(workingDirectory + "/logos/logo.png")
-	logoWidth, logoHeight = logo.size
-
 backgroundHeight, backgroundWidth, threshold = GetUserInputs()
 
 
@@ -26,36 +22,36 @@ for fileName in inputFiles:
 
 	if CheckImageExists(workingDirectory + "/input/" + fileName):
 
-		image = Image.open(workingDirectory + "/input/" + fileName)
+		with Image.open(workingDirectory + "/input/" + fileName) as image:
 
-		backgroundColour = GetBackgroundColour(image)
-		r, g, b = SeperateRGB(backgroundColour)
+			backgroundColour = GetBackgroundColour(image)
+			r, g, b = SeperateRGB(backgroundColour)
 
-		topPixel, bottomPixel, leftPixel, rightPixel = FindEdges(threshold, image, r, g, b)
+			topPixel, bottomPixel, leftPixel, rightPixel = FindEdges(threshold, image, r, g, b)
 
-		if topPixel and bottomPixel and leftPixel and rightPixel != (0, 0):
-			image = CropImage(topPixel, bottomPixel, leftPixel, rightPixel, image)
-			# Crops the image down to the edges of the image
+			if topPixel and bottomPixel and leftPixel and rightPixel != (0, 0):
+				image = CropImage(topPixel, bottomPixel, leftPixel, rightPixel, image)
+				# Crops the image down to the edges of the image
 
-		image = ScaleImage(image, backgroundWidth, backgroundHeight)
+			image = ScaleImage(image, backgroundWidth, backgroundHeight)
 
-		image = BlendBackgrounds(image, backgroundWidth, backgroundHeight)
+			image = BlendBackgrounds(image, backgroundWidth, backgroundHeight)
 
-		if CheckImageExists(workingDirectory + "/logos/logo.png", debug = False):
-			if (logoWidth <= backgroundWidth) and (logoHeight <= backgroundHeight): 
-				image.paste(logo, ((backgroundWidth - 1) - (logoWidth - 1) - 1, (backgroundHeight - 1) - (logoHeight - 1) - 1))
-				# Pastes the logo onto the bottom right corner
-				# Checks to see if the logo fits onto the background
-			else:
-				print("Logo does not fit onto background!")
+			if CheckImageExists(workingDirectory + "/logos/logo.png", debug = False):
+				with Image.open(workingDirectory + "/logos/logo.png") as logo:
+					logoWidth, logoHeight = logo.size
+					if (logoWidth <= backgroundWidth) and (logoHeight <= backgroundHeight): 
+						image.paste(logo, ((backgroundWidth - 1) - (logoWidth - 1) - 1, (backgroundHeight - 1) - (logoHeight - 1) - 1))
+						# Pastes the logo onto the bottom right corner
+						# Checks to see if the logo fits onto the background
+					else:
+						print("Logo does not fit onto background!")
 
-		image.save(workingDirectory + '/output/' + fileName, quality = 100)
-		# Saves the image to the output folder
-		image.close()
-		numEdited += 1
-		print(fileName + " has been edited and saved!")
-if CheckImageExists(workingDirectory + "/logos/logo.png", debug = False):
-	logo.close()
+			image.save(workingDirectory + '/output/' + fileName, quality = 100)
+			# Saves the image to the output folder
+
+			numEdited += 1
+			print(fileName + " has been edited and saved!")
 
 print("\n" + str(numEdited) + " files have been edited and saved!")
 # Allows the user to compare the number of edited pictures to the number of files in the input folder
