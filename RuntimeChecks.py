a07fba715a28db942614bb1e6a8c768c38d6c897
Oliver Wilkins <okwilkins@googlemail.com>
2018-06-26 15:30:43 +0100

    [Add] Code PEP8 Complient

--- RuntimeChecks.py
+++ RuntimeChecks.py
@@ -1,54 +1,87 @@
 import os
-from PIL import Image
+
 
 def GetWorkingDirectory():
-	return os.path.dirname(os.path.abspath(__file__))
-	# Gets the working directory - avoids any issues that may occur
+    return os.path.dirname(os.path.abspath(__file__))
+    # Gets the working directory - avoids any issues that may occur
+
 
 def CheckDirectoryExists(directory):
-	if not os.path.exists(directory):
-	    os.makedirs(directory)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
 
 def DirectoryCheck(directory):
-	here = GetWorkingDirectory()
-	CheckDirectoryExists(here + "/input")
-	CheckDirectoryExists(here + "/output")
-	# Checks to see if there is an input and output, if not, it creates them
+    here = GetWorkingDirectory()
+    CheckDirectoryExists(here + "/input")
+    CheckDirectoryExists(here + "/output")
+    # Checks to see if there is an input and output, if not, it creates them
+
 
 def GetInputFiles():
-	return os.listdir(GetWorkingDirectory() + "/input")
+    return os.listdir(GetWorkingDirectory() + "/input")
+
 
 def GetUserInputs():
-	while True:
-		try:
-			backgroundHeight = int(input("\nPlease enter the height (in pixels) for your edited images: "))
-			if backgroundHeight > 10000 or backgroundHeight < 1:
-				raise ValueError
+    while True:
+        try:
+            backgroundHeight = int(
+                input(
+                    "\nPlease enter the height (in pixels) "
+                    "for your edited images: "
+                )
+            )
+            if backgroundHeight > 10000 or backgroundHeight < 1:
+                raise ValueError
 
-			backgroundWidth = int(input("Please enter the width (in pixels) for your edited images: "))
-			if backgroundWidth > 10000 or backgroundWidth < 1:
-				raise ValueError
+            backgroundWidth = int(
+                input(
+                    "Please enter the width (in pixels) "
+                    "for your edited images: "
+                )
+            )
+            if backgroundWidth > 10000 or backgroundWidth < 1:
+                raise ValueError
 
-			print("\n0 is very unsensisitive and 1 is very sensitive (use a value close to 1 if the object is close in colour to the background)")
-			tolerance = float(input("Please enter a tolerance between 0 and 1 (recommened 0.8 for most images): "))
-			if tolerance > 1 or tolerance < 0:
-				raise ValueError
-				
-			print("\n")
-			break
-		except ValueError:
-			print("\nOops! That was not a valid number. Try again...")
-			# Throws an error if the user did not enter an integer
+            print(
+                "\n0 is very unsensisitive and 1 is very sensitive "
+                "(use a value close to 1 if the object is close in colour "
+                "to the background)"
+            )
+            tolerance = float(
+                input(
+                    "Please enter a tolerance between 0 and 1 "
+                    "(recommened 0.8 for most images): "
+                )
+            )
+            if tolerance > 1 or tolerance < 0:
+                raise ValueError
 
-	return backgroundHeight, backgroundWidth, tolerance
+            print("\n")
+            break
+        except ValueError:
+            print("\nOops! That was not a valid number. Try again...")
+            # Throws an error if the user did not enter an integer
+    return backgroundHeight, backgroundWidth, tolerance
 
-def GetProductCode(fileName):
-	return (fileName.split("-"))[0]
 
-def CheckImageExists(directory, debug = True):
-		if os.path.exists(directory) and (directory.endswith(".jpg") or directory.endswith(".jpeg") or directory.endswith(".png")):
-			return True
-		else:
-			if debug:
-				print(directory + " cannot be found or the image is not in the JPEG or PNG format!")
-			return False
+def CheckImageExists(directory, debug=True):
+        if (
+            os.path.exists(directory) and
+            (
+                (
+                    directory.endswith(".jpg") or
+                    directory.endswith(".jpeg") or
+                    directory.endswith(".png")
+                )
+            )
+        ):
+            return True
+        else:
+            if debug:
+                print(
+                    directory +
+                    " cannot be found or the image "
+                    "is not in the JPEG or PNG format!"
+                )
+            return False
